2db2c7e603018df68001e9c45d8bba075927590d
Vinicius Dias <carlosv775@gmail.com>
2023-09-02 15:17:56 -0300

    Video 2.2

--- chave_valor.php
+++ chave_valor.php
@@ -4,3 +4,7 @@ declare(strict_types=1);
 
 $redis = new Redis();
 $redis->connect('chave_valor');
+
+$redis->hMSet('carrinho:2', ['item' => 3, 'qtd' => 2]);
+
+echo $redis->hGet('carrinho:2', 'qtd');
