c3bfc22d5241ed20c61545aa6b3d62383b0957a7
Vinicius Dias <carlosv775@gmail.com>
2023-09-02 15:57:18 -0300

    Video 3.2

--- em_memoria.php
+++ em_memoria.php
@@ -4,3 +4,7 @@ declare(strict_types=1);
 
 $memcached = new Memcached();
 $memcached->addServer('em_memoria', 11211);
+
+// $memcached->set('chave', 'Valor 2', 10);
+
+echo $memcached->get('chave');
