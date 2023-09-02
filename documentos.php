1fc815b9384413cc707f1b8c633644b812cc1e79
Vinicius Dias <carlosv775@gmail.com>
2023-09-02 16:57:19 -0300

    Video 4.2

--- documentos.php
+++ documentos.php
@@ -2,9 +2,26 @@
 
 declare(strict_types=1);
 
+use MongoDB\BSON\ObjectId;
 use MongoDB\Client;
 
 require_once 'vendor/autoload.php';
 
 $mongodb = new Client('mongodb://usuario:senha@documentos');
 $database = $mongodb->selectDatabase('e_commerce');
+
+$colecaoDeProdutos = $database->selectCollection('produtos');
+/*$resultado = $colecaoDeProdutos->insertOne([
+    'name' => 'TV Fictícia',
+    'descricao' => 'Uma TV muito bonita',
+    'polegadas' => 40
+]);
+
+echo "Foram inseridos {$resultado->getInsertedCount()} itens. Id da última inserção: {$resultado->getInsertedId()}";
+*/
+
+$produto = $colecaoDeProdutos->findOne([
+    '_id' => new ObjectId('64f390cc390d1dbe5e050d62')
+]);
+
+echo "Essa TV tem {$produto->polegadas} polegadas";
