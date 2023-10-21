bb675c8d7319f18ec74e801819c5779d873fbca0
Vinicius Dias <carlosv775@gmail.com>
2023-10-21 18:17:35 -0300

    Curso 2 - Video 1.4

--- busca_textual.php
+++ busca_textual.php
@@ -8,10 +8,37 @@ $client = \Elastic\Elasticsearch\ClientBuilder::create()
     ->setHosts(['http://busca_textual:9200'])
     ->build();
 
-$response = $client->indices()->create([
+/* $response = $client->indices()->create([
     'index' => 'meu_indice',
 ]);
 
 var_dump($response);
+
+echo $response;
+
+$documento = [
+    'nome' => 'Vinicius Dias',
+    'usuario' => 'cviniciussdias'
+];
+
+$response = $client->index([
+    'index' => 'meu_indice',
+    'type' => 'usuarios',
+    'body' => $documento
+]);*/
+
+$response = $client->search([
+    'index' => 'meu_indice',
+    'type' => 'usuarios',
+    'body' => [
+        'query' => [
+            'match' => [
+                'nome' => 'Vinicius'
+            ]
+        ]
+    ]
+]);
+
+var_dump($response['hits']['hits']);
 
 echo $response;
