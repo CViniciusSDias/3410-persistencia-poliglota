b07006a90b39b4044c6fdb608b44ebe83fc75b59
Vinicius Dias <carlosv775@gmail.com>
2023-10-23 23:14:22 -0300

    Curso 2 - Video 3.2

--- mensageria_consumidor.php
+++ mensageria_consumidor.php
@@ -16,10 +16,16 @@ $connection = new AMQPStreamConnection(
 $channel = $connection->channel();
 $channel->queue_declare('product_bought', auto_delete: false);
 
-$msg = new AMQPMessage('Outra mensagem');
-$channel->basic_publish($msg, '', 'product_bought');
+$channel->basic_consume('product_bought', no_ack: true, callback: function (AMQPMessage $message) {
+    echo "[x] Mensagem recebida: " . $message->getBody() . PHP_EOL;
+    $message->ack();
+});
 
-echo "Mensagem enviada \n";
+try {
+    $channel->consume();
+} catch (\Throwable $exception) {
+    var_dump($exception);
+}
 
 $channel->close();
 $connection->close();
