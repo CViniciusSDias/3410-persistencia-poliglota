e5d83f9fab01f9699ee5e1e35f598933460bd8fd
Vinicius Dias <carlosv775@gmail.com>
2023-10-24 00:17:15 -0300

    Curso 2 - Video 4.3

--- grafos.php
+++ grafos.php
@@ -2,11 +2,23 @@
 
 declare(strict_types=1);
 
+use Laudis\Neo4j\ClientBuilder;
+use Laudis\Neo4j\Contracts\TransactionInterface;
+use Laudis\Neo4j\Databags\Statement;
+
 require_once 'vendor/autoload.php';
 
-$client = \Laudis\Neo4j\ClientBuilder::create()
+$client = ClientBuilder::create()
     ->withDriver('bolt', 'bolt://neo4j:12345678@grafos:7687')
     ->withDefaultDriver('bolt')
     ->build();
 
-var_dump($client->verifyConnectivity());
+// var_dump($client->verifyConnectivity());
+// $result = $client->run('CREATE (u:Usuario {nome: $nome})', ['nome' => 'Vinicius']);
+
+$client->writeTransaction(static function (TransactionInterface $transaction) {
+    $transaction->runStatements([
+        Statement::create('CREATE (u:Usuario {nome: $nome})', ['nome' => 'Patricia']),
+        Statement::create('CREATE (u:Usuario {nome: $nome})', ['nome' => 'Rafaela']),
+    ]);
+});
